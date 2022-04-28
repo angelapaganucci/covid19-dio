6be91c944083e414607ab6099d317aef71f095c0
Angela Paganucci <angela.luiz@db1.com.br>
2022-04-27 23:23:10 -0300

    Aula 02

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from 'react'
+import { StylesProvider } from '@material-ui/styles'
+import { CssBaseline } from '../node_modules/@material-ui/core/index'
+import GlobalStyle from './commons/styles/global-style'
 
 function App() {
-  return <div className="App"></div>
+  return (
+    <StylesProvider injectFirst>
+      <CssBaseline />
+      <GlobalStyle />
+      <div></div>
+    </StylesProvider>
+  )
 }
 
 export default App
---
+++ src/commons/styles/global-style.js
@@ -0,0 +1,35 @@
+import { createGlobalStyle } from '../../../node_modules/styled-components/dist/styled-components.cjs'
+import CovidImg from '../assets/images/covid.jpg'
+
+const globalStyle = createGlobalStyle`
+* {
+    outline: nome;
+    box-sizing: border-box;
+}
+
+body {
+    line-height: normal999999
+
+html, body {
+    width: 100%
+    min-height: 100%
+    display: flex
+    padding: 0
+    margin; 0
+}
+
+#root {
+    background: url(${CovidImg})
+    height: 100%
+    width: 100%
+    background-size: cover
+    bacground-position: center center
+}
+
+.mb-2 {
+    margin-
+}
+
+`
+
+export default globalStyle
